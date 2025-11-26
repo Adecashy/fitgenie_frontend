11db2a2fbfb65c92149910b16bd78de7e6403c99
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-11-26 12:27:01 +0100

    updated

--- src/pages/dashboard./DashboardIndex.jsx
+++ src/pages/dashboard./DashboardIndex.jsx
@@ -1,16 +1,122 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { userContext } from '../../contexts/UserContext'
+import { toast } from 'sonner'
 
 const DashboardIndex = () => {
   const navigate = useNavigate()
   const startPlanBuildUp = () => {
     navigate("/dashboard/generate-plan")
   }
+  const token = JSON.parse(localStorage.getItem("token"))
+  const [workoutPlan, setWorkoutPlan] = useState([])
+  const [openIndex, setOpenIndex] = useState(null)
+  const { profile, getUserProfile } = useContext(userContext)
+
+  useEffect(()=>{
+    getLatestFitPlan()
+    getUserProfile()
+  }, [])
+
+  console.log(profile)
+  
+  const toggleAccordion = (index) => {
+    setOpenIndex(openIndex === index ? null : index)
+  }
+
+  const generateFitPlan = async () => {
+    try {
+      const res = await fetch("http://localhost:7707/api/fit-plan/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      })
+      const result = await res.json()
+      if (result.success) {
+        toast.success(result.message)
+      } else if (!result.success) {
+        toast.error(result.message)
+        // navigate("/dashboard/history")
+      }
+      // console.log(result)
+    } catch (error) {
+      console.log(error)
+      toast.error(result.message || "SOMETHING WENT WRONG")
+    }
+  }
+
+  const getLatestFitPlan = async () => {
+    try {
+        const res = await fetch("http://localhost:7707/api/fit-plan/latest", {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`
+          }
+        })
+        const result = await res.json()
+        console.log(result)
+        console.log(result.plan.weeklyPlan[0].workout)
+        setWorkoutPlan(result.plan.weeklyPlan[0].workout)
+      } catch (error) {
+        console.log(error)
+      }
+      // console.log(workoutPlan[0].exercises[0].name)
+    }
   return (
     <div>
       <h1>WELCOME TO FITGENIE</h1>
       <p>click on the start button to build your plan</p>
       <button onClick={startPlanBuildUp}>Start</button>
+
+      <div className='introduction'>
+        <h2><span>Your</span> Fitness Plans</h2>
+        {profile && (
+          <p>{profile.goal} Plan  <span>{profile.subscription.status}</span></p>
+        )}
+      </div>
+
+      <div className='workout-wrapper'>
+        {profile && (
+          <h2><span>PLAN:</span> {profile.goal} plan - {profile.subscription.plan} </h2>
+        )}
+        {
+          workoutPlan.length > 0 ? (
+            <div className='workout-con'>
+              {
+                workoutPlan.map((ele, index) => (
+                  <div className='week-data' key={index}>
+                      <div className='display-menu' onClick={()=> toggleAccordion(index)}>
+                        <h3>{ele.day}</h3>
+                        <p>EXERCISES</p>
+                      </div>
+                      {openIndex === index && (
+                        <div className='item-con'>{ele.exercises.map((ele, index) =>(
+                          <div className='item-menu' key={index}>
+                            <p>{ele.name}</p>
+                            <div className='set-rep'>
+                              <p>SETS: {ele.sets}</p>
+                              <p>REPS: {ele.reps}</p>
+                            </div>
+                          </div>
+                      ))}</div>
+                      )}
+                  </div>
+                ))
+              }
+            </div>
+          ) : (
+            <div className='generate-action'>
+              <h1>No fitness plan yet</h1>
+              <p>Start by creating a personalized fitness and diet plan tailored to your specific goals and needs</p>
+              <button onClick={()=> generateFitPlan()}>Create Your First Plan</button>
+            </div>
+          )
+        }
+
+      </div>
     </div>
   )
 }
--- src/pages/dashboard./History.jsx
+++ src/pages/dashboard./History.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const History = () => {
+  const token = JSON.parse(localStorage.getItem("token"))
+  const navigate = useNavigate()
+
+  const initializeSubscription = async (plan) => {
+    try {
+      const res = await fetch("http://localhost:7707/api/subscription/initialize", {
+        method: "POST",
+        body: JSON.stringify({plan}),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      })
+      const result = await res.json()
+      console.log(result)
+      console.log(result.data.authorization_url)
+      navigate(`${result.data.authorization_url}`)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>History Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aspernatur nemo voluptas error illo quas vero et cum, obcaecati ab commodi odio a deleniti laudantium alias hic iure magni in laboriosam accusamus doloribus voluptatibus sit culpa ad? Modi eius possimus consectetur illum rem rerum iste. Vitae laudantium laborum ullam repellendus laboriosam quisquam vel amet aliquam obcaecati vero rem recusandae consequuntur mollitia inventore iure eius, nulla suscipit accusantium expedita, quas numquam voluptas quam? Non modi eveniet aliquam nemo quidem et excepturi doloremque officiis veniam cumque, nesciunt quos blanditiis. Quos deserunt expedita debitis, itaque excepturi, asperiores vitae perspiciatis fugiat quas veniam natus facere dolore cupiditate cum. Nisi, fugit necessitatibus recusandae aut atque, deserunt repellat exercitationem quod unde hic expedita placeat nostrum animi numquam voluptatem officia quam molestias explicabo temporibus sint inventore consectetur tempore incidunt porro? Id ducimus nihil eligendi amet at praesentium ipsam minus. Vero exercitationem, at expedita esse ab et numquam reiciendis quaerat voluptate dolorem repellendus officia non provident impedit consequatur, excepturi vitae eius tenetur iusto aut! Voluptatum molestiae, ea inventore ratione, dolorum atque suscipit rerum officia possimus velit, error in iste vitae distinctio delectus sunt libero! Temporibus ducimus cum facilis, optio repudiandae ullam velit? Ad tenetur doloremque laboriosam aperiam incidunt.</div>
+    <div>
+      <h1>UPGRADE TO PREMIUM</h1>
+      <div className='plan-con'>
+        <h3>Choose your subscription plan</h3>
+        <div className='plans-wrapper'>
+          <div className='each-plan-con'>
+            <p>Monthly</p>
+            <span>#10,000.00</span>
+            <ul>
+              <li>Basic Meal Plan</li>
+              <li>Limited AI Coach Access</li>
+              <li>Weekly Progress Tracking</li>
+              <li>Email Support Only</li>
+            </ul>
+            <button onClick={()=>{
+              initializeSubscription("monthly")
+            }}>Get Started</button>
+          </div>
+          <div className='each-plan-con'>
+            <p>Quarterly</p>
+            <span>#28,000.00</span>
+            <ul>
+              <li>Full Meal Plan with Alternatives</li>
+              <li>Unlimited AI Coach Chat</li>
+              <li>Advanced Progress Tracking</li>
+              <li>Priority Support</li>
+              <li>Merch + Supplements</li>
+            </ul>
+            <button onClick={()=>{
+              initializeSubscription("quarterly")
+            }}>Get Started</button>
+          </div>
+          <div className='each-plan-con'>
+            <p>Yearly</p>
+            <span>#99,999.00</span>
+            <ul>
+              <li>Full Year Multi-Phase Fitness Program</li>
+              <li>Custom Meal Plans Every Month</li>
+              <li>Unlimited AI Coach Chat</li>
+              <li>1-on-1 Monthly Check-Ins</li>
+              <li>Exclusive Programs + Full Form Analysis</li>
+              <li>Free Annual Fitness Report</li>
+              <li>Merch + Supplements + premium rewards</li>
+            </ul>
+            <button onClick={()=>{
+              initializeSubscription("yearly")
+            }}>Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
